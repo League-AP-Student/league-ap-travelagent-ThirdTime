0a0d1d9eb8443964d4ef5ff035711802b12a1441
Esme <esmepuzio@gmail.com>
2020-03-21 10:27:37 -0700

    My Solution

--- src/Trip.java
+++ src/Trip.java
@@ -4,12 +4,27 @@ public class Trip {
 	private List<Flight> flights;
 
 	public int getDuration() {
-		int duration = 0;
-		return duration;
+		if (flights.get(0) == null) {
+			return 0;
+		} else {
+			return flights.get(0).getDeparturetime().minutesUntil(flights.get(flights.size() - 1).getArrivalTime());
+		}
 	}
 
 	public int getShortestLayover() {
-		int shortest = -1;
-		return shortest;
+		if (flights.get(1) == null) {
+			return -1;
+		} else {
+			int shortestLayover = 999999999;
+			int currentInterval = 0;
+			for(int i = 0; i < flights.size();i++) {
+				if(i < flights.size()-1)
+					currentInterval = flights.get(i).getArrivalTime().minutesUntil(flights.get(i+1).getDeparturetime());
+					if(currentInterval < shortestLayover) {
+						shortestLayover = currentInterval;
+					}
+			}
+			return shortestLayover;
+		}
 	}
 }
